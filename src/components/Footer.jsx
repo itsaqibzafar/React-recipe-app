b56351d9d5bc7d4e2c66b6b64b3a3d62c286e718
itsaqibzafar <itsaqibzafar@gmail.com>
2023-02-23 00:03:37 +0500

    initial

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import css from '../components/Footer.module.css'
+const Footer = () => {
+   return (
+      <div className={css.footer}>
+         <p className={css.footerText}>&#169; aqibzafar </p>
+      </div>
+   )
+}
+
+export default Footer
